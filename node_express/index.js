da6830741f22ce620bc2a1467a8c6ff31a956c22
Seven Li-后会有七 <vinson7lhl@126.com>
2020-11-08 16:31:24 +0800

    提交

--- node_express/index.js
+++ node_express/index.js
@@ -8,12 +8,9 @@ const path = require('path')
 const fs = require('fs')
 
 
-/**
- * 1、获取模板模块,第一个参数确定模板扩展名，第二个是模板模块.注意：views文件夹是约定，模板必须放在此处 or 其子文件夹,但也可以改
- * 2、修改默认模板路径views => pages
- * app.set('views', '你要的路径')
- */
+// 1、获取模板模块,第一个参数确定模板扩展名
 app.engine('html', require('express-art-template'))
+// 2、第二个是模板模块.注意：views文件夹是art-template约定的，模板必须放在此处 or 其子文件夹,但也可以改修改默认模板路径views => pages,app.set('views', '你要的路径')
 app.set('views', path.join(__dirname, 'pages'))
 
 
@@ -49,7 +46,7 @@ app.get('/',(req,res) => {
 		}
 		res.render('index.html', {
 			title: '模板继承',
-			content: '继承内容',
+			content1: '继承内容',
 			data_list: JSON.parse(data).data_list,
 			footer_str: '这里是footer内容'
 		})
