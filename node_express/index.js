7d63f933092ff76b8df2ca55c93dccd22133a1ef
Vinson7lhl <vinson7lhl@126.com>
2020-12-30 10:52:35 +0800

    提交

--- node_express/index.js
+++ node_express/index.js
@@ -7,27 +7,24 @@ const path = require('path')
 // 获取文件对象
 const fs = require('fs')
 
-
-// 1、获取模板模块,第一个参数确定模板扩展名,第二个是模板模块
+/**
+ * 配置模板对象
+ * 1、获取模板模块,第一个参数确定模板扩展名,第二个是模板模块
+ * 2、更改模板文件映射目录；views文件夹是art-template约定的，模板必须放在此处 or 其子文件夹,但也可以改修改默认模板路径views => pages,app.set('views', '你要的路径')
+ */
 app.engine('html', require('express-art-template'))
-// 2、更改模板文件映射目录；views文件夹是art-template约定的，模板必须放在此处 or 其子文件夹,但也可以改修改默认模板路径views => pages,app.set('views', '你要的路径')
 app.set('views', path.join(__dirname, 'pages'))
 
-
-// 获取post请求后数据插件
+/**
+ * 获取post请求后数据插件？暂时未用
+ */
 const body_parser = require('body-parser')
-
 
 /**
  * 对静态资源处理，如此才可以访问 localhost:3000/(static/stylesheets/style.css)
- * app.use('映射名',实际路径)，第一个参数可以不传，那么就可以直接访问访问 'http://localhost:3000/stylesheets/base.css'
+ * app.use(逻辑路径, 物理路径)，第一个参数可以不传，那么就可以直接访问访问 'http://localhost:3000/stylesheets/base.css'
  */
 app.use('/static', express.static(path.join(__dirname, 'public')))
-
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-
 
 /**
  * 路由
@@ -38,8 +35,8 @@ app.get('/',(req,res) => {
 	 * q2：如何查询请求中的参数
 	 * q3：中间层node如何请求后端代码
 	 * q4：如何整合多个模块，比如路由拆分
+	 * tips：因为data是从文件读取的的字符串所以还要转化为对象
 	 */
-	// 注意：读取文件后的内容都是字符串，必须转码才行。【数据库】
 	fs.readFile('./public/fake_data/fake_data.json', (err,data) => {
 		if (err) {
 			return res.status(500).send('服务器错误！')
